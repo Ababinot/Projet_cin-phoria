2c36b9398f90c188e3ef69e15922cc95e6710eb5
Ababinot <babinotarthur72@gmail.com>
2024-05-29 23:27:40 +0200

    correction pour les boutons mon compte 'reload'

--- src/assets/js/NavBarMenu.js
+++ src/assets/js/NavBarMenu.js
@@ -1,11 +1,26 @@
 export default {
-  
-    methods: {
-      handleClick() {
-        this.$router.push('/connexion');
-      },
-      reserver() {
-        this.$router.push('/reserver');
-      }
+  computed: {
+    isLoggedIn() {
+      return !!localStorage.getItem('token');
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$router.push('/connexion');
+      
+    },
+    reserver() {
+      this.$router.push('/reserver');
+      
+    },
+    goToAccount() {
+      this.$router.push('/account');
+      window.location.reload();
+    },
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/');
+      window.location.reload();
     }
   }
+}
