0ebc2654cc0bb38ab63e8bbce4374c6801b6b6fe
Ababinot <babinotarthur72@gmail.com>
2024-06-06 15:26:55 +0200

    Création/modification de compte employé

--- backend/controllers/espaceAdministrationController.js
+++ backend/controllers/espaceAdministrationController.js
@@ -1,4 +1,5 @@
 const connection = require('../config/db');
+const bcrypt = require('bcrypt');
 
 exports.getFilmEspaceAdministration= (req, res) => {
   connection.query('SELECT * FROM vue_films_intranet', (error, results) => {
@@ -27,5 +28,113 @@ exports.getNbreservationEspaceAdministration= (req, res) => {
   });
 };
 
+// Fonction pour valider le mot de passe
+function validatePassword(password) {
+  const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+  return regex.test(password);
+}
 
+// Fonction pour valider l'email
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+exports.inscriptionEmploye = (req, res) => {
+  const { email, nom, prenom, password, verif_password } = req.body;
+
+  if (!email || !nom || !prenom || !password || !verif_password) {
+    return res.status(400).json({ error: 'Tous les champs sont requis' });
+  }
+
+  if (!validateEmail(email)) {
+    return res.status(400).json({ error: 'Adresse email invalide' });
+  }
+
+  if (password !== verif_password) {
+    return res.status(400).json({ error: 'Les mots de passe ne correspondent pas' });
+  }
+
+  if (!validatePassword(password)) {
+    return res.status(400).json({ error: 'Le mot de passe doit contenir au moins une majuscule, un chiffre, un caractère spécial, et être d\'au moins 6 caractères.' });
+  }
+
+  // Vérifier si l'email existe déjà
+  connection.query('SELECT email_employe FROM employe WHERE email_employe = ?', [email], (error, results) => {
+    if (error) {
+      console.error('Erreur lors de la vérification de l\'email :', error);
+      return res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+    }
+
+    if (results.length > 0) {
+      return res.status(400).json({ error: 'L\'adresse email est déjà utilisée' });
+    }
+
+    // Hachage du mot de passe et insertion dans la base de données
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        console.error('Erreur lors du hachage du mot de passe :', err);
+        return res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+      }
+
+      const query = 'INSERT INTO `employe` (`id_employe`, `nom_employe`, `prenom_employe`, `email_employe`, `mdp_employe`, `role_employe`) VALUES (?, ?, ?, ?, ?, ?);';
+      connection.query(query, [null, nom, prenom, email, hash, 'employe'], (error, results) => {
+        if (error) {
+          console.error('Erreur lors de l\'inscription :', error.sqlMessage);
+          return res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+        }
+        return res.status(201).json({ message: 'Inscription réussie' });
+      });
+    });
+  });
+};
+
+exports.updateEmploye = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Tous les champs sont requis' });
+  }
+
+  if (!validateEmail(email)) {
+    return res.status(400).json({ error: 'Adresse email invalide' });
+  }
+
+  if (!validatePassword(password)) {
+    return res.status(400).json({ error: 'Le mot de passe doit contenir au moins une majuscule, un chiffre, un caractère spécial, et être d\'au moins 6 caractères.' });
+  }
+
+  // Vérifier si l'employé existe et a le rôle "employe"
+  const checkQuery = 'SELECT * FROM employe WHERE email_employe = ? AND role_employe = ?';
+  connection.query(checkQuery, [email, 'employe'], (error, results) => {
+    if (error) {
+      console.error('Erreur lors de la vérification de l\'employé :', error);
+      return res.status(500).json({ error: 'Erreur lors de la mise à jour' });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json('N\'a pas le rôle "employe"' );
+    }
+
+    // Hachage du nouveau mot de passe et mise à jour de la base de données
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        console.error('Erreur lors du hachage du mot de passe :', err);
+        return res.status(500).json({ error: 'Erreur lors de la mise à jour' });
+      }
+
+      const updateQuery = 'UPDATE employe SET mdp_employe = ? WHERE email_employe = ?';
+      connection.query(updateQuery, [hash, email], (error, results) => {
+        if (error) {
+          console.error('Erreur lors de la mise à jour :', error.sqlMessage);
+          return res.status(500).json({ error: 'Erreur lors de la mise à jour' });
+        }
+        if (results.affectedRows === 0) {
+          return res.status(404).json({ error: 'Employé non trouvé' });
+        }
+        return res.status(200).json({ message: 'Mise à jour réussie' });
+      });
+    });
+  });
+};
 
--- src/assets/js/EspaceAdministration.js
+++ src/assets/js/EspaceAdministration.js
@@ -7,6 +7,13 @@ export default {
       espace_administration_salle: [],
       espace_administration_avis: [],
       espace_administration_reservation_film: [],
+      email: '',
+      nom: '',
+      prenom: '',
+      password: '',
+      confirmPassword: '',
+      updateEmail: '',
+      updatePassword: '',
     };
   },
   methods: {
@@ -38,10 +45,7 @@ export default {
         const response = await fetch('http://localhost:3001/api/espace-administration-nbreservation');
         const data = await response.json();
         this.espace_administration_reservation_film = data;
-    
-        // Ajoutez le console.log ici pour vérifier les données
-        console.log('Données de réservation des films:', this.espace_administration_reservation_film);
-    
+
       } catch (error) {
         console.error('Erreur lors de la récupération des nbreservation_film :', error);
       }
@@ -80,26 +84,167 @@ export default {
       const reservations = this.espace_administration_reservation_film.map(film => film.nombre_reservations);
 
       new Chart(ctx, {
-          type: 'bar',
-          data: {
-              labels: films,
-              datasets: [{
-                  label: 'Nombre de réservations',
-                  data: reservations,
-                  backgroundColor: 'rgba(91,35,51)',
-                  borderColor: 'rgba(86,77,74)',
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  y: {
-                      beginAtZero: true
-                  }
-              }
+        type: 'bar',
+        data: {
+          labels: films,
+          datasets: [{
+            label: 'Nombre de réservations',
+            data: reservations,
+            backgroundColor: 'rgba(91,35,51)',
+            borderColor: 'rgba(86,77,74)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
           }
+        }
       });
-  }
+    },
+
+    validatePassword(password) {
+      const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+      return regex.test(password);
+    },
+    validateEmail(email) {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    },
+    inscription() {
+      console.log('Données d\'inscription envoyées :', {
+        email: this.email,
+        nom: this.nom,
+        prenom: this.prenom,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      });
+
+      if (!this.email || !this.nom || !this.prenom || !this.password || !this.confirmPassword) {
+        alert('Tous les champs doivent être remplis');
+        return;
+      }
+
+      if (!this.validateEmail(this.email)) {
+        alert('Adresse email invalide');
+        return;
+      }
+
+      if (this.password !== this.confirmPassword) {
+        alert('Les mots de passe ne correspondent pas');
+        return;
+      }
+
+      if (!this.validatePassword(this.password)) {
+        alert('Le mot de passe doit contenir au moins une majuscule, un chiffre, un caractère spécial, et être d\'au moins 6 caractères.');
+        return;
+      }
+
+      const userData = {
+        email: this.email,
+        nom: this.nom,
+        prenom: this.prenom,
+        password: this.password,
+        verif_password: this.confirmPassword
+      };
+
+      console.log('Données envoyées au serveur :', userData);
+
+      fetch('http://localhost:3001/api/inscriptionEmploye', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      })
+      .then(response => {
+        if (!response.ok) {
+          return response.text().then(text => {
+            try {
+              const json = JSON.parse(text);
+              throw new Error(json.error || 'Erreur lors de l\'inscriptionEmploye');
+            } catch {
+              throw new Error(text);
+            }
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Réponse du serveur:', data);
+        alert('Inscription réussie');
+        // Rafraîchir la page après l'inscription réussie
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('Erreur lors de l\'inscriptionEmploye:', error);
+        alert('Une erreur est survenue lors de l\'inscriptionEmploye: ' + error);
+      });
+    },
+
+    modifier() {
+      if (!this.updateEmail || !this.updatePassword) {
+        alert('Tous les champs doivent être remplis');
+        return;
+      }
+
+      if (!this.validateEmail(this.updateEmail)) {
+        alert('Adresse email invalide');
+        return;
+      }
+
+      if (!this.validatePassword(this.updatePassword)) {
+        alert('Le mot de passe doit contenir au moins une majuscule, un chiffre, un caractère spécial, et être d\'au moins 6 caractères.');
+        return;
+      }
+
+      const updateData = {
+        email: this.updateEmail,
+        password: this.updatePassword
+      };
+
+      console.log('Données de mise à jour envoyées au serveur :', updateData);
+
+      fetch('http://localhost:3001/api/updateEmploye', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateData)
+      })
+      .then(response => {
+        if (!response.ok) {
+          return response.text().then(text => {
+            try {
+              const json = JSON.parse(text);
+              throw new Error(json.error || 'Erreur lors de la mise à jour');
+            } catch {
+              throw new Error(text);
+            }
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Réponse du serveur:', data);
+        alert('Mise à jour réussie');
+        // Rafraîchir la page après la mise à jour réussie
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('Erreur lors de la mise à jour :', error);
+        alert('Une erreur est survenue lors de la mise à jour : ' + error);
+      });
+    },
+
+    handleError(message, error) {
+      console.error(message, error);
+      alert('Une erreur est survenue : ' + message);
+    }
+  
+
 
   },
   mounted() {
