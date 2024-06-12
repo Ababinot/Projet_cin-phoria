acfb1536cbc80947e3347bc28d44c2ba6eaafb24
Ababinot <babinotarthur72@gmail.com>
2024-06-12 20:29:55 +0200

    test

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+    transform: {
+      '^.+\\.vue$': 'vue-jest',
+      '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    testMatch: [
+      '<rootDir>/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+    ],
+    transformIgnorePatterns: ['/node_modules/']
+  };
+  
--- spec/testFilm.spec.js
+++ spec/testFilm.spec.js
@@ -1,30 +1,9 @@
-import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
-import fetchFilms from './ListeFilms.vue'; // Assurez-vous de spécifier le bon chemin vers votre composant
+import { mount } from '@vue/test-utils';
+import ListeFilms from '@/components/ListeFilms.vue';
 
-describe('fetchFilms', () => {
-    it('should render films correctly', () => {
-      // Données de test pour les films
-      const films = [
-        { titre: 'Film 1', description: 'Description du film 1', rating: 4 },
-        { titre: 'Film 2', description: 'Description du film 2', rating: 3 }
-      ];
-  
-      // Montage du composant FilmDisplay avec les données de test
-      const wrapper = shallowMount(fetchFilms, {
-        propsData: { films }
-      });
-  
-      // Vérification de l'affichage des films
-      const filmElements = wrapper.findAll('.film');
-      expect(filmElements).to.have.lengthOf(films.length);
-  
-      // Vérification du contenu des films
-      filmElements.forEach((filmElement, index) => {
-        expect(filmElement.find('.film-title').text()).to.equal(films[index].titre);
-        expect(filmElement.find('.film-description').text()).to.equal(films[index].description);
-        expect(filmElement.find('.film-rating').text()).to.equal(films[index].rating.toString());
-      });
-    });
+describe('ListeFilms', () => {
+  test('renders correctly', () => {
+    const wrapper = mount(ListeFilms);
+    expect(wrapper.text()).toContain('Hello, world!');
   });
-  
+});
