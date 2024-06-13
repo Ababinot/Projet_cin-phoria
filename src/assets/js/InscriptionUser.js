390b4e0701054cd99363b0cf67ec814a5aa75103
Ababinot <babinotarthur72@gmail.com>
2024-06-13 15:08:03 +0200

    test

--- src/assets/js/InscriptionUser.js
+++ src/assets/js/InscriptionUser.js
@@ -1,4 +1,9 @@
-export default {
+// src/assets/js/InscriptionUser.js
+
+const emailjs = require('emailjs-com');
+const { emailConfirm } = require('@/emailjs.config');
+
+module.exports = {
   data() {
     return {
       email: '',
@@ -79,6 +84,24 @@ export default {
       .then(data => {
         console.log('Réponse du serveur:', data);
         alert('Inscription réussie');
+
+        // Envoyer l'email de confirmation
+        const templateParams = {
+          to_email: this.email,
+          to_name: `${this.prenom} ${this.nom}`,
+          message: 'Merci de vous être inscrit sur notre site!'
+        };
+
+        emailjs.send(emailConfirm.serviceID, emailConfirm.templateID, templateParams, emailConfirm.userID)
+          .then(response => {
+            console.log('Email de confirmation envoyé!', response.status, response.text);
+            alert('Un email de confirmation a été envoyé.');
+          })
+          .catch(error => {
+            console.error('Erreur lors de l\'envoi de l\'email de confirmation:', error);
+            alert('Une erreur est survenue lors de l\'envoi de l\'email de confirmation.');
+          });
+
         // Rafraîchir la page après l'inscription réussie
         window.location.reload();
       })
